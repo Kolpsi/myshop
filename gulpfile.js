374451cb42715c29a2d5ea7dc4bf0e6dfedc77b4
Kolpsi <tykataf@gmail.com>
2019-08-25 21:25:34 +0500

    Минификация

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ var autoprefixer = require("gulp-autoprefixer");
 var server = require("browser-sync").create();
 var rename = require("gulp-rename");
 var csso = require('gulp-csso');
+var del =  require("del");
+var webp = require('gulp-webp');
+var imagemin = require("gulp-imagemin");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var jsmin = require('gulp-jsmin');
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -15,18 +21,24 @@ gulp.task("css", function () {
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
         }))
     .pipe(csso())
+    .pipe(rename("style-min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
+});
+
+gulp.task("js", function () {
+  return gulp.src('source/js/*.js')
+    .pipe(jsmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('build/js'));
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -39,3 +51,58 @@ gulp.task("server", function () {
 
 gulp.task("start", gulp.series("css", "server"));
 
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/*.ico",
+    "source/css/owl.carousel.min.css"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "js",
+  "html",
+  "images",
+  "webp"
+));
+
+gulp.task("start", gulp.series("build", "server"));
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
